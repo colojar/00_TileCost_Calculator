59859fd0564925ae7726d82744598d6d15023c03
Jonathan Ardenoy <jonathan@coloweb.be>
2025-04-24 00:32:35 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_TileCost_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
