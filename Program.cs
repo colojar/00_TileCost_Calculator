7610ef029c83d61481e0454a59edebc79abca606
Jonathan Ardenoy <jonathan@coloweb.be>
2025-04-24 00:59:30 +0900

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace _00_TileCost_Calculator
+﻿// Description
+
+// Ask the user to enter in width, length, and the cost per 1 unit of flooring. Have the program calculate how much it would cost to cover the area specified with the flooring.
+
+// Added Difficulty: Calculate how much flooring would be needed for non-rectangular rooms.
+// Also figure out how much labor costs would be given that the average flooring team can only put in 20 square feet of flooring per hour at a cost of $86.00/hr.
+// Pick ONE ADDITIONAL SHAPE (triangle / circle / etc) and implement the second shape, making the user select wich one they want to calculate (time for an if statement!)
+
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace _00_TileCost_Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Constants
+            const int AREA_PER_HOUR = 20; // square feet
+            const Double LABOR_COST_PER_HOUR = 86.00; // dollars
+
+            // Variables
+            Double width = 0;
+            Double length = 0;
+            Double costPerUnit = 0;
+            Double area = 0;
+
+            // Welcome
+            Console.WriteLine("Welcome to the Tile Cost Calculator!");
+
+            // User input
+            Console.WriteLine("This program will calculate how much it will cost to cover the area of a room with flooring.");
+            Console.Write("Please enter the width of the room in feet:");
+            width = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter the length of the room in feet:");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter the cost per unit of flooring in dollars:");
+            costPerUnit = Convert.ToDouble(Console.ReadLine());
+            area = width * length;
+
+            // Calculate
+            Console.WriteLine($"The area of the room is {area} square feet.");
+            Console.WriteLine($"The cost of flooring for the room is {area * costPerUnit} dollars.");
+            Console.WriteLine($"The labor cost for the room is {area / AREA_PER_HOUR * LABOR_COST_PER_HOUR} dollars.");
+            Console.WriteLine($"The total cost for the room is {(area * costPerUnit) + (area / AREA_PER_HOUR * LABOR_COST_PER_HOUR)} dollars.");
         }
     }
 }
