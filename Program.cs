8817421b1d8609a2a801b4153b4e54abcfc80491
Jonathan Ardenoy <jonathan@coloweb.be>
2025-04-24 10:54:08 +0900

    Refactoring
    Use contansts for shapes
    Declare and assign some variable inline

--- Program.cs
+++ Program.cs
@@ -6,9 +6,6 @@
 // Also figure out how much labor costs would be given that the average flooring team can only put in 20 square feet of flooring per hour at a cost of $86.00/hr.
 // Pick ONE ADDITIONAL SHAPE (triangle / circle / etc) and implement the second shape, making the user select wich one they want to calculate (time for an if statement!)
 
-using System.ComponentModel.DataAnnotations;
-using System.Security.Principal;
-
 namespace _00_TileCost_Calculator
 {
     internal class Program
@@ -18,52 +15,52 @@ namespace _00_TileCost_Calculator
             // Constants
             const int AREA_PER_HOUR = 20; // square feet
             const Double LABOR_COST_PER_HOUR = 86.00; // dollars
-
-            // Variables
-            Double width = 0;
-            Double length = 0;
-            Double costPerUnit = 0;
-            Double area = 0;
-            String shape = "";
+            // TODO: use a list or array to store the shapes
+            const string SHAPE_RECTANGLE = "rectangle";
+            const string SHAPE_CIRCLE = "circle";
+            const string SHAPE_TRIANGLE = "triangle";
 
             // Welcome
             Console.WriteLine("Welcome to the Tile Cost Calculator!");
+            Console.WriteLine("This program will calculate how much it will cost to cover the area of a room with flooring.");
 
             // User input
-            Console.WriteLine("This program will calculate how much it will cost to cover the area of a room with flooring.");
+            Double costPerUnit = 0;
             Console.Write("Please enter the cost per unit of flooring in dollars: ");
             costPerUnit = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the shape of the room (Rectangle, Circle or Triangle): ");
+            String shape = "";
+            Console.WriteLine($"Enter the shape of the room ('{SHAPE_RECTANGLE}', '{SHAPE_CIRCLE}' or '{SHAPE_TRIANGLE}'): ");
             shape = Console.ReadLine().ToLower();
 
             // Make sure the user enters a valid shape
-            if (shape != "rectangle" && shape != "circle" && shape != "triangle")
+            // TODO: use a function to validate the shapes
+            if (shape != SHAPE_RECTANGLE && shape != SHAPE_CIRCLE && shape != SHAPE_TRIANGLE)
             {
-                Console.WriteLine("Invalid shape. Please enter either 'rectangle', 'circle' or 'triangle'.");
+                Console.WriteLine($"Invalid shape. Please enter either '{SHAPE_RECTANGLE}', '{SHAPE_CIRCLE}' or '{SHAPE_TRIANGLE}'.");
                 return;
             }
-
+            Double area = 0;
             // Shape it up
-            if (shape == "circle")
+            if (shape == SHAPE_CIRCLE)
             {
                 Console.Write("Please enter the radius of the room in feet: ");
-                width = Convert.ToDouble(Console.ReadLine());
-                area = Math.PI * Math.Pow(width, 2);
+                Double radius = Convert.ToDouble(Console.ReadLine());
+                area = Math.PI * Math.Pow(radius, 2);
             }
-            if (shape == "triangle")
+            if (shape == SHAPE_TRIANGLE)
             {
                 Console.Write("Please enter the base of the room in feet: ");
-                width = Convert.ToDouble(Console.ReadLine());
+                Double triangleBase = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Please enter the height of the room in feet: ");
-                length = Convert.ToDouble(Console.ReadLine());
-                area = (width * length) / 2;
+                Double triangleHeight = Convert.ToDouble(Console.ReadLine());
+                area = (triangleBase * triangleHeight) / 2;
             }
-            if (shape == "rectangle")
+            if (shape == SHAPE_RECTANGLE)
             {
                 Console.Write("Please enter the width of the room in feet: ");
-                width = Convert.ToDouble(Console.ReadLine());
+                Double width = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Please enter the length of the room in feet: ");
-                length = Convert.ToDouble(Console.ReadLine());
+                Double length = Convert.ToDouble(Console.ReadLine());
                 area = width * length;
             }
 
